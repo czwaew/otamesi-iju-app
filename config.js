f3bcedcf23df7665c5f4abce1b71168fe5dbf267
czwaew <cozyc69@gmail.com>
2026-08-08 20:36:25 +0900

    Add runtime config for KAMO LIFE API

---
+++ config.js
@@ -0,0 +1,9 @@
+// KAMO LIFE runtime configuration
+// Cloudflare Worker等をデプロイした後、apiBaseにそのURLを設定してください。
+// 例: https://kamo-life-api.example.workers.dev
+window.KAMO_CONFIG = {
+  apiBase: "",
+  officialAkiyaUrl: "https://www.homes.co.jp/akiyabank/niigata/kamo/",
+  officialEventUrl: "https://www.city.kamo.niigata.jp/event/",
+  officialMigrationUrl: "https://www.city.kamo.niigata.jp/ijyu/"
+};
